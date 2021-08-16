749748be421bf2458e707e18bafaed0a0c1b5754
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-17 00:12:11 +0900

    Feat : "Call New Site" Get 방식으로 받아서 Template Html 호출

--- src/main/java/Lungnaha/MyFirstSpring/FirstConnect.java
+++ src/main/java/Lungnaha/MyFirstSpring/FirstConnect.java
@@ -1,4 +1,14 @@
 package Lungnaha.MyFirstSpring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FirstConnect {
+    @GetMapping("dj") // Get 방식을 받기 위해서 사용하는 기능
+    public  String hello(Model model){
+        model.addAttribute("data", "Love..."); //Model을 활용해서 해당 html에 Key-Value 넘겨주기
+        return "firstSite.html"; // 해당 Get 방식이 들어왔을 때, 호출하려는 Html 파일 적기
+    }
 }
--- src/main/java/Lungnaha/MyFirstSpring/MyFirstSpringApplication.java
+++ src/main/java/Lungnaha/MyFirstSpring/MyFirstSpringApplication.java
@@ -3,11 +3,10 @@ package Lungnaha.MyFirstSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Spring Boot 실행 시에 Tomcat을 활용해서 같이 실행시켜주는 기능
 public class MyFirstSpringApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MyFirstSpringApplication.class, args);
 	}
 
