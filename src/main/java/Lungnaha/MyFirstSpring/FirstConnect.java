f803d4367c6fb862a3e74fec140836a61a429cab
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-17 00:36:19 +0900

    Feat : "Call New Site" Get 방식에 원하는 값을 담아서 전달 전달 받은 값을 그대로 html에서 활용

--- src/main/java/Lungnaha/MyFirstSpring/FirstConnect.java
+++ src/main/java/Lungnaha/MyFirstSpring/FirstConnect.java
@@ -3,12 +3,20 @@ package Lungnaha.MyFirstSpring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstConnect {
     @GetMapping("dj") // Get 방식을 받기 위해서 사용하는 기능
-    public  String hello(Model model){
+    public  String Hello(Model model){
         model.addAttribute("data", "Love..."); //Model을 활용해서 해당 html에 Key-Value 넘겨주기
         return "firstSite.html"; // 해당 Get 방식이 들어왔을 때, 호출하려는 Html 파일 적기
+    }
+
+    @GetMapping("myName")
+    //localhost:8080/myName?name=렁나하 이렇게 요청되면 렁나하가 name 변수에 담기는 원리
+    public  String HelloName(@RequestParam("name") String name, Model model){ //RequestParam으로 해당 Key에 맞는 값을 String name에 넣고 html에 전달
+        model.addAttribute("name", name);
+        return "MyName.html";
     }
 }
