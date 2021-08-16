a5311391510a7d2e068d09fd7463ba3c49885ba2
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-17 01:25:43 +0900

    Feat : "Interface Function" Interface를 활용한 구체적 동작 구현

---
+++ src/main/java/Lungnaha/MyFirstSpring/Service/DataInsertMemory.java
@@ -0,0 +1,35 @@
+package Lungnaha.MyFirstSpring.Service;
+
+import Lungnaha.MyFirstSpring.Domain.Member;
+import Lungnaha.MyFirstSpring.Interface.DBInterface;
+
+import java.util.*;
+
+// 이전에 구현한 Interface의 기능을 구체적으로 구현
+public class DataInsertMemory implements DBInterface {
+    private static Map<Long, Member> temp = new HashMap<>(); // id와 데이터를 따로 담기 위해 사용
+    private static long num = 0L; // id를 독립적으로 처리하기 위해 사용
+
+    @Override
+    public Member Save(Member member) {
+        member.setId(++num); // id는 자동으로 증가하도록 설정
+        temp.put(member.getId(),member);
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        for(int i = 1; i <= temp.size();i++){
+            if(temp.get(i).getName() == name) {
+                Member turn = temp.get(i);
+                return Optional.ofNullable(turn);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(temp.values());
+    }
+}
