858a9718aa1baab86b4df970635d367194485a14
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-19 17:44:43 +0900

    Feat : "Mysql 연동" JDBC를 활용해서 Mysql 연동 성공

--- src/main/java/Lungnaha/MyFirstSpring/MyFirstSpringApplication.java
+++ src/main/java/Lungnaha/MyFirstSpring/MyFirstSpringApplication.java
@@ -3,8 +3,17 @@ package Lungnaha.MyFirstSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication // Spring Boot 실행 시에 Tomcat을 활용해서 같이 실행시켜주는 기능
 public class MyFirstSpringApplication {
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		System.out.println("현재시각 : " + new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstSpringApplication.class, args);
